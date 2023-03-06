45267a1acdfd59cc64cefa6a6e18e27c9407e51a
Trilly <tttrillyyy@gmail.com>
2023-03-06 14:45:01 +0900

    javascript syntax

---
+++ syntax/template.js
@@ -0,0 +1,13 @@
+
+var name = '⭐️⭐️⭐️';
+var letter1 = 'Dear ' + name + '\n\
+\
+ Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ' + name + 'exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ' + name + 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est ' + name + 'laborum.';
+
+
+// template literal
+var letter2 = `Dear ${name}
+
+ Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ${name} exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ${name} Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est ${name} laborum.`;
+
+ console.log(letter2);
---
+++ syntax/variable.js
@@ -0,0 +1,4 @@
+var a = 1;
+console.log(a);
+a = 10;
+console.log(a);
